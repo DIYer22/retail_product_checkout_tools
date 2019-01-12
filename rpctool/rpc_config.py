1ed2f5872a986ef638188f7b52f2ab5d1221e067
Lei <ylxx@live.com>
2019-01-13 00:04:06 +0800

    add interface

--- rpctool/rpc_config.py
+++ rpctool/rpc_config.py
@@ -7,10 +7,10 @@ Created on Thu Dec 27 22:07:37 2018
 """
 
 from boxx import dicto
-
-
 config = dicto({
     "debug": False,
+    "ROUND_NUM": 2,
+    "column_names": ['method', 'diff', 'cAcc', 'mCIoU', 'ACD', 'mCCD', 'mAP50', 'mmAP']
 })
 
 
---
+++ rpctool/utils.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: DIYer22@github
+@mail: yanglei@megvii.com
+Created on Wed Jan  9 16:37:20 2019
+"""
+from boxx import *
+
+def res_bbox_to_counter(resJs):
+    pass
+    
+    
+def gt_bbox_to_counter(annJs):
+    pass
+
+
+if __name__ == "__main__":
+    pass
+    
+    
+    
